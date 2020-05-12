25824cc35de09bbe430b5a776f81ae47af394341
satjava <satyendradv956@gmail.com>
2020-05-12 17:25:14 +0530

    add student reg form

--- vintec/app/src/main/java/com/example/vintec/AddStudents.java
+++ vintec/app/src/main/java/com/example/vintec/AddStudents.java
@@ -1,11 +1,14 @@
 package com.example.vintec;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddStudents extends AppCompatActivity {
     SessionManager session;
+    EditText edittext= (EditText) findViewById(R.id.issue_dt);
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +17,6 @@ public class AddStudents extends AppCompatActivity {
         getSupportActionBar().setTitle("Vintec Computer Education");
         getSupportActionBar().setSubtitle("Add New Certificate");
         session = new SessionManager(AddStudents.this.getApplicationContext());
+
     }
 }
